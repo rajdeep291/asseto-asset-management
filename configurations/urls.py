bc3fc5d0d76a37aa32d1a12e22eb7c35209cce9f
Rajdeep <rajdeep.291@vyrazu.com>
2025-10-23 13:41:25 +0530

    changed sidebar

--- configurations/urls.py
+++ configurations/urls.py
@@ -6,19 +6,28 @@ app_name = 'configurations'
 
 urlpatterns=[
     path('upload-logo/',views.logo_upload,name='upload_logo'),
+
     path('delete-logo/<int:id>',views.delete_logo,name='delete_logo'),
+
     path('delete-favicon/<int:id>',views.delete_favicon,name='delete_favicon'),
+
     path('delete-login-page-logo/<int:id>',views.delete_login_page_logo,name='delete_login_page_logo'),
+
+    
+    
+    
+    
     path('update-tag-configuration/<str:id>/', views.create_or_update_tag_configuration, name='update_tag_configuration'),
+
     path('update-tag-configuration/', views.create_or_update_tag_configuration, name='update_tag_configuration_without_id'),
+
     # path('update-tag-configuration/<str:id>/', views.update_tag_configuration, name='update_tag_configuration'),
     path('list-tag-configuration/', views.list_tag_configurations, name='list_tag'),
+
     path('toggle-default-settings/<str:id>/', views.toggle_default_settings, name='toggle_default_settings'),
-    path('delete-favicon/<int:id>',views.delete_favicon,name='delete_favicon'),
-    path('delete-login-page-logo/<int:id>',views.delete_login_page_logo,name='delete_login_page_logo'),
-    path('update-tag-configuration/<str:id>/', views.create_or_update_tag_configuration, name='update_tag_configuration'),
-    path('update-tag-configuration/', views.create_or_update_tag_configuration, name='update_tag_configuration_without_id'),
-    # path('update-tag-configuration/<str:id>/', views.update_tag_configuration, name='update_tag_configuration'),
-    path('list-tag-configuration/', views.list_tag_configurations, name='list_tag'),
-    path('toggle-default-settings/<str:id>/', views.toggle_default_settings, name='toggle_default_settings'),
+
+
+
+
+
 ]
